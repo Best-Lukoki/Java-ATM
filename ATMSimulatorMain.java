b3e12c4715559234e76c495ba4bb813fb83ef592
Best Lukoki <ec25901@qmul.ac.uk>
2026-06-10 13:11:20 +0100

    added main method

---
+++ ATMSimulatorMain.java
@@ -0,0 +1,7 @@
+public class ATMSimulatorMain {
+    public static void main (String [] args) {
+        ATMSimulator a = new ATMSimulator("");
+        a.inputName();
+        a.simulation();
+    }
+}
